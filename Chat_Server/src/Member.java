4b27f3a00a254613db4c04ca2a71bb20a14f32ca
mye0ngs00 <ourendpoint@gmail.com>
2021-01-09 18:10:30 +0900

    Update the Member

--- Chat_Server/src/Member.java
+++ Chat_Server/src/Member.java
@@ -1,32 +1,30 @@
-/*  # 여러 개의 소켓을 관리할 수있는 Member 클래스를 따로 만들어 socket 과 입력받을 닉네임을 저장할 Map 객체를 생성하여 관리한다. */
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+/* 채팅 링크를 관리하는 클래스 */
 public class Member {
-	//맵생성
+	// Map(소켓:학번)
 	private Map<Socket, String> mb;
 	
 	public Member() {
-		//해시맵으로 전환
 		mb = new HashMap<Socket, String>();
 	}
-	//get하면 map반환
-	public Map<Socket, String> getMb() {
-		return mb;
-	}
-	//해시맵에 소켓이랑 학번 추가
+	
+	// 소켓이랑 학번 추가
 	public void addMember(Socket member, String user_id) {
 		this.mb.put(member, user_id);
 	}
-	//멤버삭제
+	
+	// 멤버 링크 삭제
 	public void delMember(Socket p) {
 		if (mb.size() == 0) {
 			System.out.println("삭제 할 멤버가 없음");
-			return;
 		}else {
 			mb.remove(p);
 			System.out.println(p.getInetAddress() + "멤버가 삭제되었습니다.");
 		}
 	}
+	
+	public Map<Socket, String> getMb() { return mb; }
 }
