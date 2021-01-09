78450c89aa0a8fd0667c99b32c44b4aa93fa30cb
mye0ngs00 <ourendpoint@gmail.com>
2021-01-09 17:31:11 +0900

    Update Crawler

--- Chat_Server/src/Crawler.java
+++ Chat_Server/src/Crawler.java
@@ -2,28 +2,31 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+/* 교내 홈페이지 파싱을 위한 크롤러 */
 public class Crawler {
-	//크롤링할 url
-	private static String univURL = "http://portal.hs.ac.kr/HsIs/index.jsp";
-	//학교 공지사항 5by6 matrix
-	private static String[][] notice_text = new String[5][7];
-	private static String[][] notice_link = new String[5][7];
+	final private String univURL = "http://portal.hs.ac.kr/HsIs/index.jsp";
+	private String[][] notice_text;
+	private String[][] notice_link;
 	
-	//생성자, 나중ㅇ ㅔ필요할 수 있어서 까먹을까봐 일단 만듦.
-	Crawler(){}
+	// 생성자
+	public Crawler() {
+		this.notice_text = new String[5][7];
+		this.notice_link = new String[5][7];
+	}
 	
-	//correction
+	// 파싱
 	public void Update() {
 		try {
 			Document doc = Jsoup.connect(univURL).get();
 			Elements notices = doc.select("ul.recentBbsInnerUl").select("li.recentBbsInnerLi");
 
-			//텍스트 + 링크
 			for(int i=0; i<notices.size(); i++) {
 				notice_text[i/7][i%7] = notices.get(i).text();
 				notice_link[i/7][i%7] = notices.get(i).select("a").attr("href");
 			}
-		}catch(Exception e) {System.out.println("crawling err");}
+		}catch(Exception e) {
+			System.out.println("crawling err");
+		}
 	}
 	
 	public String getNoticeText(int row, int col) {return notice_text[row][col];}
