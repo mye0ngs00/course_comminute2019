eb71517a6990fb52c33702417927cd8a070798b3
mye0ngs00 <ourendpoint@gmail.com>
2021-01-09 19:50:39 +0900

    Update the ServerReceiver && ServerBroadCast

--- Chat_Server/src/ServerReceiver.java
+++ Chat_Server/src/ServerReceiver.java
@@ -6,93 +6,93 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Map;
 
+/* 수신 */
 public class ServerReceiver implements Runnable {
 	private Member member;
 	private String data;
 	private BufferedReader br;	
 	private BufferedWriter bw;
-	private boolean stop = true;
+	private boolean flag = true;
 	private Socket socket;
-	
 	private Crawler crawler = null;
 	
-	//생성자
+	// 생성자
 	public ServerReceiver(Socket socket, Member member) {
 		this.socket = socket;
 		this.member = member;
-		crawler = new Crawler();
+		this.crawler = new Crawler();
 		
-		//30분에 한 번 크롤링
+		// 30분에 한 번 크롤링
 		new Thread(()->{
-			while(true)
+			while(true) {
 				try {
 					crawler.Update();
 					System.out.println("crawling complete");
 					Thread.sleep(1800000);
 				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
 					e.printStackTrace();
 				}
+			}
 		}).start();
 	}
 
-	//소켓에 메세지 싣기
+	// 소켓에 메세지 싣기
 	public void outPutMessage(Map.Entry<Socket, String> s, String data) {
 		try {
 			bw = new BufferedWriter(new OutputStreamWriter(s.getKey().getOutputStream(), "UTF-8"));
 			bw.write(data + "\n");
 			bw.flush();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("outputMessage Exception");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
-	//소켓에 받은 데이터를 읽고 출력
+	// 소켓에 받은 데이터를 읽고 출력
 	public void socketReader() {
 		try {
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-			data = br.readLine();
+			this.data = br.readLine();
+			System.out.println(data);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("socketReader Exception");
+			// 연결 끊기
 			member.delMember(socket);
-			if (socket != null)
+			if (socket != null) {
 				socketClose();
-			stop = false;
+			}
+			flag = false;
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
-		//데이터 출력
-		//System.out.println(data);
 	}
 
+	// 소켓 종료
 	public void socketClose() {
 		if (socket != null) {
 			try {
 				socket.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("socketClose Exception");
+				System.out.println(e.getMessage());
+				e.printStackTrace();
 			}
 		}
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		while (stop) {
-			// 헷갈리니 그냥 소켓 읽기로 간주하는게 나음.
+		while (flag) {
 			socketReader();
-			//help에서 송신한 메세지인지??
 			try {
+				// 데이터를 #!@ 규칙으로 정하고 파싱
 				String[] isHelp = this.data.split("#!@");
-				//flag가 왔을 경우 맨 처음 flag의 앞이 help인가?
 				if(isHelp[0].equals("help")) {
 					String commandStr = this.data.substring(7);
 					
 					try {
 						bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
-						String temp = ""; //빈 스트링
-						//유저가 보내온 메세지 확인
-						//퍼포먼스 별로 차이 안나서 그냥 switch-case문 사용. 재미로 넣은 기능
+						String temp = null;
+						// 명령어 구분
 						switch(commandStr) {
 							case "공지":	
 								for(int i=0; i< 7; i++) {
@@ -144,75 +144,86 @@ public class ServerReceiver implements Runnable {
 								bw.write("help#!@ㅡㅡㅡ링크를 클릭하시면 이동됩니다ㅡㅡㅡ\n");
 								bw.flush();
 								break;
-							case "야": bw.write("help#!@네??\n"); break;
-							case "오": bw.write("help#!@감탄사 5\n"); break;
-							case "ㅇ": bw.write("help#!@사람은 아무거나 타이핑 하고 싶을 때 'ㅇ'를 친답니다.\n"); break;
-							case "d": bw.write("help#!@사람은 아무거나 타이핑 하고 싶을 때 'd'를 친답니다.\n"); break;
-							case "음": bw.write("help#!@고민하지 마세요~\n"); break;
-							case "ㅋ": bw.write("help#!@웃기신가요!?\n"); break;
-							case "/help": bw.write("help#!@현재 공지 / 행사 / 학사 / 장학 / 취업 만 구현했으며, 이 기능은 동적 해시함수로 수정 예정입니다. 감사합니다.\n"); break;
-							default: bw.write("help#!@" + "아직 구현 안됐어요ㅜㅜ /help 를 입력해보세요~\n");
+							case "야":
+								bw.write("help#!@네??\n");
+								break;
+							case "오":
+								bw.write("help#!@감탄사 5\n");
+								break;
+							case "음":
+								bw.write("help#!@고민하지 마세요~\n");
+								break;
+							case "ㅋ":
+								bw.write("help#!@웃기신가요!?\n");
+								break;
+							case "/help":
+								bw.write("help#!@현재 공지 / 행사 / 학사 / 장학 / 취업 만 구현했습니다. 감사합니다.\n");
+								break;
+							default:
+								bw.write("help#!@" + "아직 구현 안됐어요ㅜㅜ /help 를 입력해보세요~\n");
 						}
 						bw.flush();
-						
-					}catch(Exception e) {}
+					}catch(Exception e) {
+						System.out.println(e.getMessage());
+						e.printStackTrace();
+					}
+					// help 기능만 수행하고 넘김.
 					continue;
 				}
-			}catch(Exception e) {}
-			//db저장
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+			
+			// 수신 메시지 브로드캐스팅
+			for (Map.Entry<Socket, String> s : this.member.getMb().entrySet()) {
+				if (!s.getKey().equals(socket)) {
+					String formatted_data = new String(this.data);
+					outPutMessage(s, formatted_data);
+				}
+			}
+						
+			// 채팅 기록 저장
 			try {
 				String[] db_tmp_args = this.data.split("@");
 				ServerDB.st.executeUpdate("insert into chatlog(time, sender_id, course_id, payload) "
 						+ "values('"+db_tmp_args[0] + "','" + db_tmp_args[2] + "','" + db_tmp_args[1] + "','" + db_tmp_args[3] +"');");
-			}catch(Exception e) { System.out.println("DB err");		
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();		
 			}
 			
-			//유저학번 찾기
-			String user = member.getMb().get(socket); //hash-map 의 key에 대응되는 return value.	
-			
-			// 퇴장. 
-			//TODO: 나중에 강퇴 기능으로 바꿀 것.
-			/*
-			else if (data.trim().equals("퇴장")) {
-				String user = member.getMb().get(socket);
-				System.out.println(socket.getInetAddress() + " " + user + " 님이 퇴장하셨습니다.");
-				String data = user + "님이 퇴장하셨습니다..";
-				for (Map.Entry<Socket, String> s : member.getMb().entrySet()) {
-					outPutMessage(s, data);
-				}
-				member.delMember(socket);
-				stop = false;
-				socketClose();
-			}*/
-			
-			// 기능
-			/*
-			else if (data.startsWith("@")) {
-				String user = member.getMb().get(socket);
-				// 귓속말
-				if (data.startsWith("@귓속말/")) {
-					String[] whisper = data.split("/");
-					String data = "귓속말/" + user + ": " + whisper[2];
-					for (Map.Entry<Socket, String> s : this.member.getMb().entrySet()) {
-						if (s.getValue().equals(whisper[1])) {
-							outPutMessage(s, data);
-							break;
-						}
-					}
-				} // 귓속말 종료
-			} // 기능 else if 종료. */
-			
-
-
-			
-			// 전체 대화
-			for (Map.Entry<Socket, String> s : this.member.getMb().entrySet()) {
-				if (!s.getKey().equals(socket)) {
-					//데이터양식
-					String data = new String(this.data);
-					outPutMessage(s, data);
-				}
-			}
+			// 소켓에 연결된 학번 찾기 (귓속말,퇴장 용도)
+//			String user = member.getMb().get(socket);	
+//			
+			// 퇴장
+//			
+//			else if (data.trim().equals("퇴장")) {
+//				String user = member.getMb().get(socket);
+//				System.out.println(socket.getInetAddress() + " " + user + " 님이 퇴장하셨습니다.");
+//				String data = user + "님이 퇴장하셨습니다..";
+//				for (Map.Entry<Socket, String> s : member.getMb().entrySet()) {
+//					outPutMessage(s, data);
+//				}
+//				member.delMember(socket);
+//				flag = false;
+//				socketClose();
+//			}
+//			
+			// 귓속말
+//			else if (data.startsWith("@")) {
+//				String user = member.getMb().get(socket);
+//				if (data.startsWith("@귓속말/")) {
+//					String[] whisper = data.split("/");
+//					String data = "귓속말/" + user + ": " + whisper[2];
+//					for (Map.Entry<Socket, String> s : this.member.getMb().entrySet()) {
+//						if (s.getValue().equals(whisper[1])) {
+//							outPutMessage(s, data);
+//							break;
+//						}
+//					}
+//				}
+//			}
 		} // while문 종료
 	}// run 종료
 } 
